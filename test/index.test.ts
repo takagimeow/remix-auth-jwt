e8d618de129a384f1f17ef7ba681a79dd19b2ecf
Keisuke Takagi <mit@takagi.app>
2023-02-18 10:47:07 +0000

    feat: JwtStrategy created

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
-import { createCookieSessionStorage } from "@remix-run/server-runtime";
-import { MyStrategy } from "../src";
+import { createCookieSessionStorage } from "@remix-run/node";
+import { JwtStrategy } from "../src";
 
-describe(MyStrategy, () => {
+describe(JwtStrategy, () => {
   let verify = jest.fn();
   // You will probably need a sessionStorage to test the strategy.
   let sessionStorage = createCookieSessionStorage({
@@ -13,7 +13,7 @@ describe(MyStrategy, () => {
   });
 
   test("should have the name of the strategy", () => {
-    let strategy = new MyStrategy({ something: "You may need" }, verify);
+    let strategy = new JwtStrategy({ something: "You may need" }, verify);
     expect(strategy.name).toBe("change-me");
   });
 
