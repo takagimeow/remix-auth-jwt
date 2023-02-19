1de459e7a38cac496877a05819fd3d163ce3e08a
Keisuke Takagi <mit@takagi.app>
2023-02-19 19:05:11 +0000

    fix: fix could not resolve jsonwebtoken

--- src/core/service/jsonwebtoken/impl/JsonwebtokenServiceImpl.ts
+++ src/core/service/jsonwebtoken/impl/JsonwebtokenServiceImpl.ts
@@ -1,20 +1,30 @@
 import { JsonwebtokenService } from "../JsonwebtokenService";
-import * as jwt from "jsonwebtoken";
+// import * as jwt from "jsonwebtoken";
+import type * as jsonwebtoken from "jsonwebtoken";
 
 export class JsonwebtokenServiceImpl implements JsonwebtokenService {
+  protected jwtSign: typeof jsonwebtoken.sign;
+  protected jwtVerify: typeof jsonwebtoken.verify;
+  constructor() {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires, unicorn/prefer-module
+    const jwt = require("jsonwebtoken");
+    this.jwtSign = jwt.sign;
+    this.jwtVerify = jwt.verify;
+  }
+
   verify(
     token: string,
-    secretOrPublicKey: jwt.Secret
-  ): string | jwt.JwtPayload {
-    const result = jwt.verify(token, secretOrPublicKey);
+    secretOrPublicKey: jsonwebtoken.Secret
+  ): string | jsonwebtoken.JwtPayload {
+    const result = this.jwtVerify(token, secretOrPublicKey);
     return result;
   }
   sign(
     // eslint-disable-next-line @typescript-eslint/ban-types
     payload: string | object | Buffer,
-    secretOrPrivateKey: jwt.Secret
+    secretOrPrivateKey: jsonwebtoken.Secret
   ): string {
-    const result = jwt.sign(payload, secretOrPrivateKey);
+    const result = this.jwtSign(payload, secretOrPrivateKey);
     return result;
   }
 }
